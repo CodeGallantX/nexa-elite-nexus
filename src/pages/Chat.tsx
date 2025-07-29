e7b46b04c64c3731c4134d29278f9a35080f4c94
John A. Samuel <144686157+CodeGallantX@users.noreply.github.com>
2025-07-29 14:22:11 +0100

    chat UI

--- src/pages/Chat.tsx
+++ src/pages/Chat.tsx
@@ -677,7 +677,7 @@ export const Chat: React.FC = () => {
                     onTouchStart={(e) => handleTouchStart(e, msg)}
                   >
                     <div
-                      className={`relative p-2.5 rounded-lg max-w-[95%] sm:max-w-[90%] ${
+                      className={`relative p-1.5 rounded max-w-[95%] sm:max-w-[90%] ${
                         msg.user_id === user?.id
                           ? 'chat-bubble outgoing bg-primary text-white ml-auto'
                           : 'chat-bubble incoming bg-gray-800 text-white'
